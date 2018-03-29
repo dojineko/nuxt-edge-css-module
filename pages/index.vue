d107bdcd1f6f2d9c4261aeed0d30764bfc82cbde
dojineko <dojineko.spiral@gmail.com>
2018-03-29 18:45:26 +0900

    reproduction

---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div :class="$style.text" style="font-size:32pt; color:red;">
+    EXPECT BLUE
+  </div>
+</template>
+
+<style module>
+.text {
+  color: blue !important;
+}
+</style>
